736c5047cb9e59d7fd193508ce066ab56c117646
Kevin Qin <kevin.qinzw@gmail.com>
2024-08-25 15:49:13 -0400

    test for .env

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 app.use(express.json())
 
