dacf79a9a4c3761936eea4b2a98e8fd4f51e3023
Kevin Qin <kevin.qinzw@gmail.com>
2024-08-25 16:12:09 -0400

    uibuild

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -5,13 +5,20 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const url = process.env.MONGODB_URI
-
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Note = mongoose.model('Note', noteSchema)
