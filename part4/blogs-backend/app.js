bf374c3dfb410458c538bf6059015ef6544ee00b
Kevin Qin <kevin.qinzw@gmail.com>
2024-08-28 15:13:55 -0400

    finished part4a

--- part4/blogs-backend/app.js
+++ part4/blogs-backend/app.js
@@ -9,8 +9,6 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-logger.info('connecting to', config.MONGODB_URI)
-
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
